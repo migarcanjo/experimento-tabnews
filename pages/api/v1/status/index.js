b4e36b858e42f2614feeb4d358a0727cab5aa50e
Miguel Arcanjo <133447396+migarcanjo@users.noreply.github.com>
2024-06-26 14:41:34 +0000

    Add `api/v1/status` endpoint 2

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,6 @@
 function status(request, response) {
   response
     .status(200)
-    .json({ chave: "Alunos do curso.dev são pessoas acima da média" });
+    .json({ chave: "Os alunos do curso dev são acima da média" });
 }
 export default status;
